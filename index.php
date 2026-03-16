8174e73525beafc99b1f1548949207d79106bf2f
gissellgelvis-maker <gissellgelvis@gmail.com>
2026-03-16 13:42:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+$Num1=7;
+$Num2=5;
+$sum=$Num1+$Num2;
+$rest=$Num1-$Num2;
+$mult=$Num1*$Num2;
+$div=$Num1/$Num2;
+$esp=$Num1**$Num2;
+echo "La suma es:".$sum."\n";
+echo "La resta es:".$rest."\n";
+echo "La multiplicacion es:".$mult."\n";
+echo "La division es:".$div."\n";
+echo "el exponencial es:".$esp."\n";
+?>
+
+
+
+
+
+
+
+
+
+
